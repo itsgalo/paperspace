c949a2f505233810a25afb3ce2e2587fd9c5f464
galo.canizares@gmail.com <galo.canizares@gmail.com>
2020-03-11 21:28:14 -0400

    first test

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const http = require('http');
-const express = require('express');
+const app = require('./app');
 
-const app = express();
-app.use(express.static('public'));
 app.set('port', '3000');
 
 const server = http.createServer(app);
