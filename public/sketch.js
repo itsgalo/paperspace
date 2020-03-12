b0f457bf5801c18f9403eaacff21528be4610abe
galo.canizares@gmail.com <galo.canizares@gmail.com>
2020-03-12 00:20:24 -0400

    fixed drawing background

--- public/sketch.js
+++ public/sketch.js
@@ -1,31 +1,52 @@
 let socket;
+let pg;
+let lines = [];
 
 function setup() {
   createCanvas(window.windowWidth, window.windowHeight);
   background(51);
-  socket = io.connect('http://52.206.85.248:3000');
+  socket = io.connect('http://52.207.62.235:3000');
   socket.on('mouse', newDrawing);
 }
 
+
 function newDrawing(data) {
-  noStroke();
-  fill(255, 0, 100);
-  ellipse(data.x, data.y, 20, 20);
+  lines.push(new drawLine(data.x, data.y, data.px, data.py));
 }
 
 function draw() {
-
+  background(51);
+  for (var i = 0; i < lines.length; i++) {
+    lines[i].show();
+  }
 }
 
-function mouseDragged() {
+function mouseDragged(e) {
+
+  e.preventDefault();
 
   let data = {
     x: mouseX,
-    y: mouseY
+    y: mouseY,
+    px: pmouseX,
+    py: pmouseY
   }
   socket.emit('mouse', data);
 
-  noStroke();
-  fill(255, 255, 255);
-  ellipse(mouseX, mouseY, 20, 20);
+  lines.push(new drawLine(mouseX, mouseY, pmouseX, pmouseY));
+  console.log(lines);
+
+  return false;
+}
+
+function drawLine(x, y, px, py) {
+  this.show = function() {
+    stroke(255);
+    strokeWeight(5);
+    line(x, y, px, py);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(window.windowWidth, window.windowHeight);
 }
